3327ad91116128e64c63dbbb2582dedb6abc10b1
Benjamin <benjamin.j.arias@gmail.com>
2016-06-03 16:39:54 +0200

    feat: Added onReady function to Container component

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,14 +1,27 @@
-// in src/views/Main/Container.js
 import React from 'react'
+import Map, {GoogleApiWrapper} from 'google-maps-react';
 
 export class Container extends React.Component {
+	onReady(mapProps, map) {
+
+	}
   render() {
+  	const style = {
+  		width: '100vw',
+  		height: '100vh'
+  	}
+
     return (
       <div>
-        Hello from the container
+      	Hello from the container
+        <Map 
+        onReady={this.onReady.bind(this)}
+        google={this.props.google} />
       </div>
     )
   }
 }
 
-export default Container
+export default GoogleApiWrapper({
+	apiKey: 'AIzaSyDe9DggTZ5TVlWB9S-eocVmKf30m-zx2tY'
+})(Container);
